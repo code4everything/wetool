ca09490dc223145f2fd4b35de39fae16af18440c
pantao <easepan@qq.com>
2019-07-08 10:14:51 +0800

    fix: test

--- src/test/org/code4everything/wetool/util/WeUtilsTest.java
+++ src/test/org/code4everything/wetool/util/WeUtilsTest.java
@@ -1,11 +1,14 @@
 package org.code4everything.wetool.util;
 
+import org.code4everything.wetool.config.WeConfig;
+import org.code4everything.wetool.factory.BeanFactory;
 import org.junit.Test;
 
 public class WeUtilsTest {
 
     @Test
     public void compressString() {
+        BeanFactory.register(new WeConfig());
         String test = "a         b\r\nc\rd\ne";
         assert "a b c d e".equals(WeUtils.compressString(test));
     }
