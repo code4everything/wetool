eb24012208b6bee34d777d7e5387a1230127e81a
pantao <easepan@qq.com>
2019-08-24 11:37:15 +0800

    fix: test

---
+++ src/test/org/code4everything/wetool/WeApplicationTest.java
@@ -0,0 +1,12 @@
+package org.code4everything.wetool;
+
+/**
+ * @author pantao
+ * @since 2019/8/24
+ */
+public class WeApplicationTest {
+
+    public static void main(String[] args) {
+        WeApplication.main(args);
+    }
+}
